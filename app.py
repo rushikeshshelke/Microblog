f6dbd638fd147e0750a307afa305ada7b4d58272
rushikeshshelke <shelke.rushi2010@gmail.com>
2022-10-11 11:14:10 +0530

    Hiding secrets

--- app.py
+++ app.py
@@ -1,14 +1,19 @@
+import os
+
 from flask import Flask
 from flask_restful import Api
 from resources.home import Home
 from commonLibs.initialiseLogging import InitialiseLogging
 from pymongo import MongoClient
 from commonLibs.globalvariables import GlobalVariables
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
 GlobalVariables.APP = app
 api = Api(GlobalVariables.APP)
-client = MongoClient("mongodb://rushi:Rushi2020@ac-h77fpjp-shard-00-00.g92obdv.mongodb.net:27017,ac-h77fpjp-shard-00-01.g92obdv.mongodb.net:27017,ac-h77fpjp-shard-00-02.g92obdv.mongodb.net:27017/?ssl=true&replicaSet=atlas-ht8ez8-shard-0&authSource=admin&retryWrites=true&w=majority")
+client = MongoClient(os.environ.get("MONGODB_URI"))
 GlobalVariables.APP.db = client.microblog
 
 InitialiseLogging().setupLogging()
